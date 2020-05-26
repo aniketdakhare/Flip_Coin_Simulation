d91818d431313d45e5670db11cbbce8131f21efc
“Aniket_D” <aniketdakhare@gmail.com>
2020-05-26 14:30:29 +0530

    Counted number of heads and tails

---
+++ uc2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+
+heads=0
+tails=0
+function flip()
+{
+
+	coin=$((RANDOM%2))
+	if [ $coin -eq 0 ]
+	then
+		((heads++))
+	else
+		((tails++))
+	fi
+}
+for ((i=1; i<=20; i++))
+do
+	flip
+done
+echo heads won $heads times
+echo Tails won $tails times
