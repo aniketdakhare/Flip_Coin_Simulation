9b66e0a37a8ed9471a91e653c508f37289e761c5
“Aniket_D” <aniketdakhare@gmail.com>
2020-05-26 14:39:01 +0530

    Checked who won

---
+++ uc3.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -x
+
+heads=0
+tails=0
+function flip()
+{
+
+	coin=$((RANDOM%2))
+	if [ $coin -eq 0 ]
+	then
+		((heads++))
+	else
+		((tails++))
+	fi
+}
+while [ $heads -le 21 -a $tails -le 21 ]
+do
+	flip
+done
+if [ $heads -gt $tails ]
+then
+	echo Heads Win by $(($heads-$tails))
+elif [ $tails -gt $heads ]
+then
+	echo Tails Win by $(($tails-$heads))
+else
+	echo Its Tie
+fi
+
